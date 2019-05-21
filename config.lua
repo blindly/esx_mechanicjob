9059ea74e9292b1972b45310923d241e710fcef3
unknown <rgnix2@gmail.com>
2019-05-21 15:25:59 -0400

    updated mechation location

--- config.lua
+++ config.lua
@@ -3,12 +3,12 @@ Config.Locale = "en"
 
 Config.DrawDistance = 100.0
 Config.MaxInService = -1
-Config.EnablePlayerManagement = false
+Config.EnablePlayerManagement = true
 Config.EnableSocietyOwnedVehicles = false
 
 Config.NPCSpawnDistance = 500.0
 Config.NPCNextToDistance = 25.0
-Config.NPCJobEarnings = {min = 15, max = 40}
+Config.NPCJobEarnings = {min = 100, max = 300}
 
 Config.Vehicles = {
 	"adder",
@@ -21,6 +21,7 @@ Config.Vehicles = {
 Config.Zones = {
 	MechanicActions = {
 		Pos = {x = -203.47, y = -1328.16, z = 33.85},
+		Pos = {x = 1188.02, y = 2636.09, z = 33.85},
 		Size = {x = 1.5, y = 1.5, z = 1.0},
 		Color = {r = 204, g = 204, b = 0},
 		Type = 1
