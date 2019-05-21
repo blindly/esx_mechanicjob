6551a13196fe9cad424f74480b2c84e215ccc928
unknown <rgnix2@gmail.com>
2019-05-21 15:33:26 -0400

    fixed location test

--- locales/en.lua
+++ locales/en.lua
@@ -82,5 +82,5 @@ Locales['en'] = {
   ['have_withdrawn']            = 'you have withdrawn ~y~x%s~s~ ~b~%s~s~',
   ['have_deposited']            = 'you have deposited ~y~x%s~s~ ~b~%s~s~',
   ['player_cannot_hold']        = 'you do ~r~not~s~ have enough ~y~free space~s~ in your inventory!',
-  ['mech_is_on']                = 'The is a mechanic on. Please message the mechanic',
+  ['mech_is_on']                = 'Their is a mechanic on. Please message the mechanic',
 }
